b6b1e04ff9e5fc4ec2018c0d6a8f598839d6fbe6
AntLJ <51942821+AntLJ@users.noreply.github.com>
2020-01-22 08:42:23 +0800

    Create AttributeTool.cpp

---
+++ SiNDY-e/AttributeTool/AttributeTool.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) INCREMENT P CORP. All Rights Reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY INCREMENT P CORP., WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT.
+ *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * CLAIM, DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+*/
+
+// AttributeTool.cpp : DLL エクスポートのインプリメンテーション
+
+
+// メモ: Proxy/Stub 情報
+//  別々の proxy/stub DLL をビルドするためには、プロジェクトのディレクトリで 
+//      nmake -f AttributeToolps.mak を実行してください。
+
+#include "stdafx.h"
+#include "resource.h"
+#include <initguid.h>
+#include "AttributeTool.h"
+
+#include "AttributeTool_i.c"
+#include "AttributeToolObj.h"
+#include "AttributeToolProp.h"
+#include "AttributeToolPropLQ.h"
+
+
+CComModule _Module;
+
+BEGIN_OBJECT_MAP(ObjectMap)
+OBJECT_ENTRY(CLSID_AttributeToolObj, CAttributeToolObj)
+OBJECT_ENTRY(CLSID_AttributeToolProp, CAttributeToolProp)
+OBJECT_ENTRY(CLSID_AttributeToolPropLQ, CAttributeToolPropLQ)
+END_OBJECT_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// DLL エントリ ポイント
+
+extern "C"
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /*lpReserved*/)
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        _Module.Init(ObjectMap, hInstance, &LIBID_ATTRIBUTETOOLLib);
+        DisableThreadLibraryCalls(hInstance);
+    }
+    else if (dwReason == DLL_PROCESS_DETACH)
+        _Module.Term();
+    return TRUE;    // ok
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// DLL が OLE によってアンロード可能かどうかを調べるために使用されます
+
+STDAPI DllCanUnloadNow(void)
+{
+    return (_Module.GetLockCount()==0) ? S_OK : S_FALSE;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// 要求された型のオブジェクトを作成するためにクラス ファクトリを返します
+
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
+{
+    return _Module.GetClassObject(rclsid, riid, ppv);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// DllRegisterServer - システム レジストリへエントリを追加します
+
+STDAPI DllRegisterServer(void)
+{
+    // オブジェクト、タイプライブラリおよびタイプライブラリ内の全てのインターフェイスを登録します
+    return _Module.RegisterServer(TRUE);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// DllUnregisterServer - システム レジストリからエントリを削除します
+
+STDAPI DllUnregisterServer(void)
+{
+    return _Module.UnregisterServer(TRUE);
+}
+
+

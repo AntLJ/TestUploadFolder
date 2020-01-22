4901e9c940d9c863fc5ec32f25f22971562fc174
AntLJ <51942821+AntLJ@users.noreply.github.com>
2020-01-22 08:44:17 +0800

    Add files via upload

---
+++ SiNDY-e/AttributeTool/document/intro.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) INCREMENT P CORP. All Rights Reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY INCREMENT P CORP., WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT.
+ *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * CLAIM, DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+*/
+
+/**
+ * @file	intro.h
+ * @brief	for Doxygen
+ */
+/**
+@page intro はじめに
+
+@section intro_about 機能概要
+
+SiNDYのオブジェクトのありとあらゆるものを表示するためのツール\n
+
+
+@section intro_reason 目的
+
+属性を表示するためのユーザインターフェイス部分であり、属性を表示するダイアログを表示させるためのツール。
+ダイアログの実装はこのツールの中では行っておらず、このツールはオブジェクトを選択するだけである。
+フィーチャだけでなくリンク列の属性表示も、このツールで行う。
+
+
+*/
---
+++ SiNDY-e/AttributeTool/document/tool.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) INCREMENT P CORP. All Rights Reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY INCREMENT P CORP., WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT.
+ *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * CLAIM, DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+*/
+
+/**
+ * @file	tool.h
+ * @brief	for Doxygen
+ *
+ */
+/**
+@page tool ツール仕様 
+
+@section tool_file ツールファイル
+
+<table>
+	<tr>
+		<th>AttributeTool.dll</th>
+		<th>属性表示編集ツール</th>
+	</tr>
+</table>
+
+@section tool_include インクルードファイル
+
+<table>
+	<tr>
+		<th>AttributeToolObj.h</th>
+		<th>属性表示編集ツールのメインクラス</th>
+	</tr>
+	<tr>
+		<th>AttributeToolProp.h</th>
+		<th>属性表示編集ツールのプロパティページの定義</th>
+	</tr>
+	<tr>
+		<th>AttributeToolPropLQ.h</th>
+		<th>リンク列属性表示編集ツールのプロパティページの定義</th>
+	</tr>
+</table>
+
+@section tool_specification ツール仕様詳細
+
+@htmlinclude specification.h
+
+@section tool_checklist チェックリスト
+
+@htmlinclude AttributeDlgAttributeToolCHKList.htm
+
+*/
